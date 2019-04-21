0fa064c7b8faea1d1a2bd05662e45ee34dcc5c8e
musebe <emusebe9@gmail.com>
2019-04-21 18:40:59 +0000

    Implemented the movies and actors page

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import "./Header.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
 const Header = () => {
   return (
     <div className='rmdb-header'>
       <div className='rmdb-header-content'>
-        <img className='rmdb-logo' src="./images/reactMovie_logo.png" alt="rmdb-logo" />
-        <img className='rmdb-tmdb-logo' src="./images/tmdb_logo.png" alt="tmdb-logo" />
+        <Link to='/'>
+          <img
+            className='rmdb-logo'
+            src='./images/reactMovie_logo.png'
+            alt='rmdb-logo'
+          />
+        </Link>
+        <img
+          className='rmdb-tmdb-logo'
+          src='./images/tmdb_logo.png'
+          alt='tmdb-logo'
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
