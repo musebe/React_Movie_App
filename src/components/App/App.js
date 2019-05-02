5a86c104b78b9b0cb0068bda7848f26df9562a14
musebe <emusebe9@gmail.com>
2019-05-02 11:07:22 +0000

    Added HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
 import Movie from '../Movie/Movie';
@@ -7,17 +7,17 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <React.Fragment>
         <Header />
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/:movieId" component={Movie} exact />
+          <Route path='/' component={Home} exact />
+          <Route path='/:movieId' component={Movie} exact />
           <Route component={NotFound} />
         </Switch>
       </React.Fragment>
-    </BrowserRouter>
-  )
-}
+    </HashRouter>
+  );
+};
 
 export default App;
